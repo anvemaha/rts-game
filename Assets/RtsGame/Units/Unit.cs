6bae3d470a744376cc3b1c501711ba4b596eefea
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2021-02-07 11:51:28 +0200

    Implement unit damage animation

---
+++ Assets/RtsGame/Units/Unit.cs
@@ -0,0 +1,22 @@
+﻿using RtsGame.Combat;
+using UnityEngine;
+
+namespace RtsGame.Units
+{
+    public class Unit : MonoBehaviour
+    {
+        [SerializeField] private Damageable damageable;
+        [SerializeField] private UnitAnimation unitAnimation;
+
+        private void Start()
+        {
+            damageable.Damaged += unitAnimation.TakeDamage;
+        }
+
+        [ContextMenu("Test taking damage")]
+        public void TestTakingDamage()
+        {
+            damageable.Health -= 1;
+        }
+    }
+}
---
+++ Assets/RtsGame/Units/UnitAnimation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace RtsGame.Units
+{
+    public class UnitAnimation : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        private static readonly int TakeDamageHash = Animator.StringToHash("TakeDamage");
+
+        public void TakeDamage()
+        {
+            animator.SetTrigger(TakeDamageHash);
+        }
+    }
+}
