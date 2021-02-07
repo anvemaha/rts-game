dd56e858313517958c4ee084190d9ef8f5fd10ca
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2021-02-07 11:39:34 +0200

    Add Damaged event & fix Death
    Death is now emitted only when health goes to 0.

--- Assets/RtsGame/Tests/DamageSystemTests.cs
+++ Assets/RtsGame/Tests/DamageSystemTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using NUnit.Framework;
 using RtsGame.Combat;
 using UnityEngine;
 
@@ -7,7 +9,7 @@ namespace RtsGame.Tests
     [TestFixture]
     public class DamageSystemTests
     {
-        [Test, Description("Test that damageable loses health amount equal to damager's attack damage")]
+        [Test, Description("Test that damageable loses health amount equal to damager\'s attack damage")]
         public static void TakingDamage()
         {
             var damageableGo = new GameObject();
@@ -35,6 +37,47 @@ namespace RtsGame.Tests
             damageable.Health = 100;
             damageable.Health = 0;
             Assert.IsTrue(wasCalled);
+        }
+        
+        [Test, Description("Test that damageable doesn't fire Death event when health stays above 0")]
+        public static void NotDying()
+        {
+            var damageableGo = new GameObject();
+            var damageable = damageableGo.AddComponent<Damageable>();
+            bool wasCalled = false;
+            damageable.Death += () => wasCalled = true;
+            damageable.Health = 100;
+            damageable.Health -= 10;
+            Assert.IsFalse(wasCalled);
+        }
+        
+        [Test, Description("Test that damageable fires an event when health is reduced")]
+        public static void Damaged()
+        {
+            var damageableGo = new GameObject();
+            var damageable = damageableGo.AddComponent<Damageable>();
+            bool wasCalled = false;
+            damageable.Damaged += () => wasCalled = true;
+            damageable.Health = 100;
+            damageable.Health -= 10;
+            Assert.IsTrue(wasCalled);
+        }
+
+        [Test, Description("Test that damageable fires both damaged and death events when health is reduced to 0")]
+        public static void DamagedWhenDying()
+        {
+            var damageableGo = new GameObject();
+            var damageable = damageableGo.AddComponent<Damageable>();
+            List<string> events = new List<string>();
+
+            damageable.Damaged += () => events.Add("Damaged");
+            damageable.Death += () => events.Add("Death");
+            
+            damageable.Health = 100;
+            damageable.Health = 0;
+
+            List<string> expected = new List<string>() {"Damaged", "Death"};
+            CollectionAssert.AreEqual(expected, events);
         }
     }
 }
