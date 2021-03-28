240dbde8c9aa42c026ef08d29ac8d0c489d571d9
anttiharju <git@anttiharju.dev>
2021-03-28 15:49:48 +0300

    Implement factions
    Can only command units within the same faction.
    Cannot attack units in the same faction.

---
+++ Assets/RtsGame/Faction.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace RtsGame
+{
+    [CreateAssetMenu(fileName = "Faction", menuName = "ScriptableObjects/Faction", order = 1)]
+    public class Faction : ScriptableObject
+    {
+        
+    }
+}
