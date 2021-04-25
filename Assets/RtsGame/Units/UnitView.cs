c71489764e1f34306bd9a3253c7d59a755fc6221
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2021-04-25 15:22:16 +0300

    Visualize selected unit with a blue ring

---
+++ Assets/RtsGame/Units/UnitView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace RtsGame.Units
+{
+    public class UnitView : MonoBehaviour
+    {
+        [SerializeField] private Unit unit;
+        [SerializeField] private GameObject selectionIndicator;
+
+        private void Awake()
+        {
+            unit.OnSelectionChanged += ToggleSelectionIndicator;
+            selectionIndicator.SetActive(false);
+        }
+
+        private void ToggleSelectionIndicator(bool selected)
+        {
+            selectionIndicator.SetActive(selected);
+        }
+    }
+}
