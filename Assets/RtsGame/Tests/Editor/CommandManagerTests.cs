229c03d87987fa7e783f6ecaa96f49ef8c9288aa
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2021-04-25 15:46:24 +0300

    Test that own units can be commanded

--- Assets/RtsGame/Tests/Editor/CommandManagerTests.cs
+++ Assets/RtsGame/Tests/Editor/CommandManagerTests.cs
@@ -10,7 +10,7 @@ namespace RtsGame.Tests.Editor
     public class CommandManagerTests
     {
         [Test]
-        public static void TestThatOnlyUnitsOfOwnFactionCanBeCommanded()
+        public static void EnemyUnitsCanNotBeCommanded()
         {
             EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
             var friendlyFaction = ScriptableObject.CreateInstance<Faction>();
@@ -30,6 +30,30 @@ namespace RtsGame.Tests.Editor
             rtsInputStub.TriggerActionOnUnit(friendlyUnit);
 
             Assert.IsEmpty(commandExecutorStub.commands);
+        }
+
+        [Test]
+        public static void OwnUnitsCanBeCommanded()
+        {
+            EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
+            var friendlyFaction = ScriptableObject.CreateInstance<Faction>();
+            var enemyFaction = ScriptableObject.CreateInstance<Faction>();
+
+            var commandExecutorStub = new CommandExecutorStub();
+            var rtsInputStub = new RtsInputStub();
+            var selectionSystem = new SelectionSystem(rtsInputStub, friendlyFaction);
+            new CommandManager(friendlyFaction, rtsInputStub, commandExecutorStub, selectionSystem);
+
+            var friendlyUnit = new GameObject().AddComponent<Unit>();
+            var enemyUnit = new GameObject().AddComponent<Unit>();
+            friendlyUnit.Faction = friendlyFaction;
+            enemyUnit.Faction = enemyFaction;
+
+            rtsInputStub.TriggerSelectOnUnit(friendlyUnit);
+            rtsInputStub.TriggerActionOnUnit(enemyUnit);
+
+            Assert.AreEqual(1, commandExecutorStub.commands.Count);
+            Assert.IsInstanceOf<AttackCommand>(commandExecutorStub.commands[0]);
         }
 
     }
