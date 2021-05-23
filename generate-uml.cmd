e2a88f34e08b2062fa84f4e46fb2be95e9e876c6
anvemaha <antti.v.harju@gmail.com>
2021-05-23 13:57:21 +0300

    Silence UML script

--- generate-uml.cmd
+++ generate-uml.cmd
@@ -1,10 +1,11 @@
-: Generates UML diagram and opens it as a .png.
+: Generates UML diagram as a .svg file and opens it.
 : 
 : DEPENCIES:
 : https://github.com/pierre3/PlantUmlClassDiagramGenerator
 : Has to be installed locally. Follow the instructions in the repository.
 : https://plantuml.com/
-: MIT-licensed version is included, but you might need things like java.
-puml-gen ./Assets/ ./Documentation/Generated/ -dir -createAssociation -allInOne
-java -jar ./Tools/PlantUML/plantuml.jar ./Documentation/Generated/include.puml
-start ./Documentation/Generated/include.png
+: MIT-licensed version is included, but you might need things like Java.
+@echo off
+puml-gen ./Assets/ ./Documentation/Generated/ -dir -createAssociation -allInOne > nul
+java -jar ./Tools/PlantUML/plantuml.jar ./Documentation/Generated/include.puml -tsvg
+start ./Documentation/Generated/include.svg
